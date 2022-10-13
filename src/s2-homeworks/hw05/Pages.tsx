d6932225758ff3fd491558027c4191ec29e43926
OlgaKapskaya <olikbuko@gmail.com>
2022-10-13 17:16:38 +0300

    fix errors (HW5)

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -19,8 +19,8 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={'/'} element={<PreJunior/>}/>
-
+                {/*<Route path={'/'} element={<PreJunior/>}/>*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
