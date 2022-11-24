510ac73efab0a3d9f5db38a89dd60ae004212912
OlgaKapskaya <olikbuko@gmail.com>
2022-11-24 12:09:31 +0300

    HW12 + change CSS HW11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -29,7 +29,13 @@ const SuperRange: React.FC<SliderProps> = (props) => {
                 '& .MuiSlider-track': {
                     height: 3,
                 },
-            }}
+                '& .MuiSlider-thumb:after':
+                    { width: '6px',
+                        height: '6px',
+                        top: '50%',
+                        left: '50%',
+                        backgroundColor: '#00CC22  '
+            }}}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
